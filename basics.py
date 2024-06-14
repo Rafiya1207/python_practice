dbff686bd11857d567222756f43a4953b0e67a19
Rafiya1207 <rafiya1207@gmail.com>
2024-06-14 17:08:37 +0530

    list operations

--- basics.py
+++ basics.py
@@ -1,8 +1,13 @@
 #lists
 a = [1,2,'@$',"apple",9.87]
-print(a)
-for i in a:
-    if i == 'apple':
-        break
-    print(i)
-    
+print("original -->",a)
+a.append(1995)
+print("append-->",a)
+a.remove('@$')
+print("remove-->",a)
+a.insert(4,'@')
+print("insert element at 4th index -->",a)
+a.sort
+print("sorted list -->",a)
+a.pop()
+print("pop-->",a)
